cfd436442ec7984ec96a85e3882d8dcdf5593ed9
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2021-10-28 18:15:53 +0200

    added html js

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
-function getWeatherFromAPI(city) {
-  var weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Berlin&appid=d85e1324298ab6b05c6dc20c2cc8da56&units=metric` ;
+// Setting up the local storage; get search history from local storage
+const searchHistory = JSON.parse(localStorage.getItem('search-history')) || []
 
-  fetch(weatherApiUrl)
+// DOM nodes
+const cityNode = $("#city")
+const cityList = $("#city-list")
+
+function saveHistory (history){
+    localStorage.setItem("search-history", JSON.stringify(history))
+}
+
+// Implement wheater API with fetch fuction; getting the wheater data for the city
+function getWeatherFromAPI(city) {
+  const weatherApiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=6&appid=${"d85e1324298ab6b05c6dc20c2cc8da56"}&units=metric` ;
+
+  return fetch(weatherApiUrl)
     .then(function (weatherApiResponse) {
       return weatherApiResponse.json();
-    }).then(function(weatherData){
-    console.log(weatherData)
     })
 }
 
+function showHistory(history){
+    cityList.empty()
+    for (const city of history){
+        cityList.append($(
+            `<button class="showWheaterButton">${city}</button>`
+        ))
+    }
+    $(".showWheaterButton").click(showWheaterCity)
+}
+
+// implement the main fuction
+function showWheaterCity(event){
+    console.log(event.target)
+
+}
+
+function showWeather(data){
+
+}
 
 $("#check").on("click", function(){
-    getWeatherFromAPI();
+    const city = cityNode.val()
+    searchHistory.push(city)
+    saveHistory(searchHistory)
+
+
+    getWeatherFromAPI(city).then(function (result){
+        showWeather(result)
+        showHistory(searchHistory)
+    })
 
 })
+showHistory(searchHistory)
 
-    console.log("Hello")
+console.log("Hello")
