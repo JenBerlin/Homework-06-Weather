b0a557b6871a76f4c535df424fdf996e815e8c8d
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2021-10-31 15:38:23 +0100

    adjustments mainly on the js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// var iconcode = a.weather[0].icon;
+
 // Setting up the local storage; get search history from local storage
 const searchHistory = JSON.parse(localStorage.getItem('search-history')) || []
 
@@ -10,9 +12,8 @@ function saveHistory (history){
 }
 
 // Implement wheater API with fetch fuction; getting the wheater data for the city
-
 const getWeatherFromAPI = async(city) => {
-  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Berlin&appid=d85e1324298ab6b05c6dc20c2cc8da56&units=metric`;
+  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=d85e1324298ab6b05c6dc20c2cc8da56&units=metric`;
 
 try {
     const response = await fetch(weatherApiUrl)
@@ -20,11 +21,7 @@ try {
     console.log(data)
     return data
 } catch (error) {
-    
-}
-
-function currentDayCard(data) {
-$("#temp11").innerHTML(data.list[0].main.temp)
+    console.error(error.message)
 }
 
 //   return fetch(weatherApiUrl)
@@ -34,6 +31,14 @@ $("#temp11").innerHTML(data.list[0].main.temp)
 //     }).catch(function(error){
 //         console.log(error)
 //     })
+}
+
+function currentDayCard(data) {
+$("#temp11").text(data.list[0].main.temp)
+$("#wind11").text(data.list[0].wind.speed)
+$("#humidity11").text(data.list[0].main.humidity)
+// Call same API different end point for UV
+// $("#uv11").text(data.list[0].main.humidity)
 }
 
 function showHistory(history){
@@ -48,7 +53,9 @@ function showHistory(history){
 
 // implement the main fuction
 function showWheaterCity(event){
-    console.log(event.target)
+    getWeatherFromAPI(event.target.innerHTML).then(function (result){
+        currentDayCard(result)
+    })
 }
 
 function showWeather(data){
@@ -57,14 +64,17 @@ function showWeather(data){
 
 $("#check").on("click", function(){
     const city = cityNode.val()
+if (!searchHistory.includes(city)){
     searchHistory.push(city)
+}
+
     saveHistory(searchHistory)
 
     getWeatherFromAPI(city).then(function (result){
         showWeather(result)
         showHistory(searchHistory)
+        currentDayCard(result)
     })
-
 })
 showHistory(searchHistory)
 
